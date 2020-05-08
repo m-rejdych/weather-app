414d6885ccb4ae26455af02e36c09a2a0ad76e48
Michał Rejdych <rejdych.m.r@gmail.com>
2020-05-08 16:56:27 +0200

    Fixed major bugs

--- src/modules/_fetchData.js
+++ src/modules/_fetchData.js
@@ -5,7 +5,7 @@ async function fetchData() {
   const { inputText, resultPageInfo, inputRadio1, background } = elements;
 
   try {
-    const dataUrl = `http://api.openweathermap.org/data/2.5/weather?q=${inputText.value}&appid=2a2d45553be4f6fe1eeea72d749597e0`;
+    const dataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputText.value}&appid=2a2d45553be4f6fe1eeea72d749597e0`;
 
     const response = await fetch(dataUrl, { mode: 'cors' });
     const json = await response.json();
